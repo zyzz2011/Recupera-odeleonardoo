8069f08b38a2fe9d0dd33367a9d96cb71174d857
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-02 12:31:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+   System.out.println("digite seu nome");
+   String nome = sc.nextLine();
+  System.out.println("digite seu sobrenome");
+   String sobrenome = sc.next();
+   System.out.println("digite sua idade");
+   Double idade = sc.nextDouble();
+   System.out.println("digite sua altura");
+  Double alt = sc.nextDouble();
+  System.out.println("digite seu peso");
+  Double peso = sc.nextDouble();
+
+sc.close();
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  }
+
+ 
+}
